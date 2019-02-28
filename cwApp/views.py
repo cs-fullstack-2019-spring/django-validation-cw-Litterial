f9daefcf8ae99165b10adc1c63e0c3f81cf0c76e
Litterial <yungice12@gmail.com>
2019-02-28 12:46:33 -0600

    I broke something Kenn and I added comments

--- cwApp/views.py
+++ cwApp/views.py
@@ -4,13 +4,15 @@ from .forms import CarForm
 # Create your views here.
 
 def index(request):
+    #if the user submitted a post
     if request.method == 'POST':
         form=CarForm(request.POST)
+        #if there are no errors create the post , create a new car model and go to the victory page
         if form.is_valid():
             Car.objects.create(make=request.POST['make'],model=request.POST['model'],year=request.POST['year'],mph=request.POST['mph'])
 
             return render(request,'cwApp/congrats.html')
-
+         #otherwise list errors
         else:
             allEntries=Car.objects.all
             context={
@@ -19,7 +21,7 @@ def index(request):
                     'form':form,
                 }
             return render (request,'cwAPP/index.html',context)
-
+    # code to carry information to index template
     allEntries=Car.objects.all()
     form=CarForm()
     context={
